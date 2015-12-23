ad019c1d222e505848dc2b2a8cacac1f317a366f
James Youngquist <jay@cs.uw.edu>
2015-12-22 23:22:03 -0800

    Cache mask to /tmp

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
@@ -12,6 +13,7 @@
 #include <unistd.h>
 
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <memory>
@@ -45,6 +47,15 @@ cppmpl::CppMatplotlib MplConnect (std::string config_path);
 
 
 constexpr uint32_t GIMX_UDP_BUF_SIZE = 158;
+std::string MASK_IMAGE_FILE = "/tmp/mask.png";
+
+bool FileExists (std::string filename) {
+  std::ifstream file_check(filename);
+  bool found = file_check.good();
+  file_check.close();
+  return found;
+}
+
 
 class GimxController {
 public:
@@ -57,10 +68,12 @@ private:
   virtual void SerializeTo_ (int32_t *axes) const = 0;
 };
 
+
 struct Stick {
   int32_t x = 0;
   int32_t y = 0;
 };
+
 
 class XboneControl : public GimxController {
 public:
@@ -74,6 +87,7 @@ public:
     axes[3] = right_stick.y;
   }
 };
+
 
 class GimxConnection {
 public:
@@ -143,6 +157,7 @@ private:
   struct sockaddr_in server_;
   int32_t sock_fd_;
 };
+
 
 namespace fc = FlyCapture2;
 class Flea3 {
@@ -214,6 +229,11 @@ public:
     //cv::Mat image(tmp_image, cv::Rect(10, 110, 620, 60));
     cv::Mat image;
     cv::resize(tmp_image, image, cv::Size(0, 0), SCALE, SCALE, cv::INTER_CUBIC);
+    return image;
+  }
+
+  cv::Mat GetImageFloat () {
+    cv::Mat image = GetImage();
     image.convertTo(image, CV_32F);
     return image;
   }
@@ -241,6 +261,52 @@ cppmpl::CppMatplotlib MplConnect (std::string config_path="") {
   return mpl;
 }
 
+namespace std {
+// This specialization lets us use descriptors as keys in a map
+template<>
+struct less<cv::Mat> {
+   bool operator()(const cv::Mat& A, const cv::Mat& B) const {
+     assert(A.rows == B.rows);
+     assert(A.cols == B.cols);
+     assert(A.type() == B.type());
+     assert(A.channels() == 1);
+
+     for (int row = 0; row != A.rows; ++row) {
+       for (int col = 0; col != A.cols; ++col) {
+         switch (A.type()) {
+         case CV_32F:
+           if (A.at<float>(row, col) > B.at<float>(row, col))
+             return false;
+           if (A.at<float>(row, col) < B.at<float>(row, col))
+             return true;
+           break;
+
+         case CV_64F:
+           if (A.at<double>(row, col) > B.at<double>(row, col))
+             return false;
+           if (A.at<double>(row, col) < B.at<double>(row, col))
+             return true;
+           break;
+
+         case CV_8UC1:
+           if (A.at<uint8_t>(row, col) > B.at<uint8_t>(row, col))
+             return false;
+           if (A.at<uint8_t>(row, col) < B.at<uint8_t>(row, col))
+             return true;
+           break;
+
+         default:
+           throw std::runtime_error("Unsupported type");
+           break;
+         }
+       }
+     }
+
+     // All elements equal, but say we're less to avoid loops?
+     return true;
+   }
+};
+}
 
 int main (int argc, char **argv) {
   (void) argc;
@@ -261,53 +327,58 @@ int main (int argc, char **argv) {
   ctl.right_stick.x = 25000;
   gimx.SendControl(ctl);
 
-  // Compute mask
-  std::cout << "Creating motion mask. Takes about 10 seconds." << std::endl;
-  std::vector<cv::Mat> images;
-  for (size_t capture_count = 0; capture_count != 600; ++capture_count) {
-    images.push_back(flea3.GetImage() / 255.0);
-    cv::imshow("image", images.back());
-    cv::waitKey(1);
-    for (size_t skip_count = 0; skip_count != 1; ++skip_count) {
-      //flea3.GetImage();
-    }
-  }
-
-  // Deltas between successive images
-  std::cout << "  Computing deltas" << std::endl;
-  std::vector<cv::Mat> diffs;
-  cv::Mat mean_diff = images[0].clone() * 0;
-  for (size_t idx = 1; idx != images.size(); ++idx) {
-    cv::Mat diff = images[idx] - images[idx-1];
-    mean_diff += diff;
-    diffs.push_back(diff);
-  }
-  mean_diff /= diffs.size();
-
-  // Stdev of the deltas
-  std::cout << "  Computing stdev" << std::endl;
-  cv::Mat std_diff = diffs[0].clone() * 0;
-  for (size_t idx = 0; idx != diffs.size(); ++idx) {
-    cv::Mat deviation = diffs[idx] - mean_diff;
-    std_diff += deviation.mul(deviation);
-  }
-  cv::sqrt(std_diff / diffs.size(), std_diff);
-  double min, max;
-  cv::minMaxLoc(std_diff, &min, &max);
-  std_diff = (std_diff - min) / (max - min);
-
-  // Create the mask
   cv::Mat mask;
-  cv::GaussianBlur(std_diff, mask, cv::Size(0, 0), 1.0);
-  cv::threshold(mask, mask, 0.25, 1.0, cv::THRESH_BINARY);
-  cv::erode(mask, mask, cv::getStructuringElement(cv::MORPH_RECT,
-                                                  cv::Size(3, 3)));
-  mask *= 255;
-  mask.convertTo(mask, CV_8UC1);
+  if (!FileExists(MASK_IMAGE_FILE)) {
+    // Compute mask
+    std::cout << "Creating motion mask. Takes about 10 seconds." << std::endl;
+    std::vector<cv::Mat> mask_images;
+    for (size_t capture_count = 0; capture_count != 600; ++capture_count) {
+      mask_images.push_back(flea3.GetImageFloat() / 255.0);
+      cv::imshow("image", mask_images.back());
+      cv::waitKey(1);
+      for (size_t skip_count = 0; skip_count != 0; ++skip_count) {
+        flea3.GetImage();
+      }
+    }
 
-  cv::imshow("image", mask);
-  cv::waitKey(0);
-  exit(0);
+    // Deltas between successive mask_images
+    std::cout << "  Computing deltas" << std::endl;
+    std::vector<cv::Mat> diffs;
+    cv::Mat mean_diff = mask_images[0].clone() * 0;
+    for (size_t idx = 1; idx != mask_images.size(); ++idx) {
+      cv::Mat diff = mask_images[idx] - mask_images[idx-1];
+      mean_diff += diff;
+      diffs.push_back(diff);
+    }
+    mean_diff /= diffs.size();
+
+    // Stdev of the deltas
+    std::cout << "  Computing stdev" << std::endl;
+    cv::Mat std_diff = diffs[0].clone() * 0;
+    for (size_t idx = 0; idx != diffs.size(); ++idx) {
+      cv::Mat deviation = diffs[idx] - mean_diff;
+      std_diff += deviation.mul(deviation);
+    }
+    cv::sqrt(std_diff / diffs.size(), std_diff);
+    double min, max;
+    cv::minMaxLoc(std_diff, &min, &max);
+    std_diff = (std_diff - min) / (max - min);
+
+    // Create the mask
+    cv::GaussianBlur(std_diff, mask, cv::Size(0, 0), 1.0);
+    cv::threshold(mask, mask, 0.25, 1.0, cv::THRESH_BINARY);
+    cv::erode(mask, mask, cv::getStructuringElement(cv::MORPH_RECT,
+                                                    cv::Size(3, 3)));
+    mask *= 255;
+    mask.convertTo(mask, CV_8UC1);
+
+    cv::imwrite(MASK_IMAGE_FILE, mask);
+
+    cv::imshow("image", mask);
+    cv::waitKey(0);
+  } else {
+    mask = cv::imread(MASK_IMAGE_FILE);
+  }
 
   // capture loop
   char key = 0;
@@ -316,8 +387,8 @@ int main (int argc, char **argv) {
   double diff_us = 0;
   double counter = 0;
   auto detdes_ptr = cv::BRISK::create();
-  images.clear();
-  for (size_t capture_count = 0; key != 'q' && capture_count < 20;
+  std::vector<cv::Mat> images;
+  for (size_t capture_count = 0; key != 'q' && capture_count < 5;
        ++capture_count) {
     struct timeval time;
     gettimeofday(&time, NULL);
@@ -341,81 +412,109 @@ int main (int argc, char **argv) {
   }
 
   // Now extract feature tracks
+  // Pass 1, figure out all descriptors of interest.
+  std::cout << "Extracting feature tracks" << std::endl;
 
-  // Key is index into the first
-  std::map<int, std::vector<cv::KeyPoint>> feature_map;
-
+  cv::Mat image_0 = images[0];
   std::vector<cv::KeyPoint> keypoints_0;
   cv::Mat descriptor_0;
-  detdes_ptr->detectAndCompute(images[0], mask, keypoints_0,
+  detdes_ptr->detectAndCompute(image_0, mask, keypoints_0,
                                descriptor_0, false);
   if(descriptor_0.type() != CV_32F) {
     descriptor_0.convertTo(descriptor_0, CV_32F);
   }
+  cv::FlannBasedMatcher matcher;
+  matcher.add(descriptor_0);
 
-  for (size_t idx = 1; idx != images.size(); ++idx) {
+  std::vector<cv::Mat> all_descriptors;
 
+  // Key is index into the first
+  std::cout << "  Pass 1" << std::endl;
+  for (int idx = 0; idx != descriptor_0.rows; ++idx) {
+    all_descriptors.push_back(descriptor_0.row(idx));
   }
 
-
-  std::vector<std::array<double, 4>> motion_vecs;
-  if (false) {
-    cv::Mat image_1 = flea3.GetImage();
-    cv::Mat image_2 = flea3.GetImage();
-
-    std::vector<cv::KeyPoint> keypoints_1;
-    detdes_ptr->detect(image_1, keypoints_1);
-
-    cv::Mat descriptor_1;
-    detdes_ptr->compute(image_1, keypoints_1, descriptor_1);
-    if(descriptor_1.type() != CV_32F) {
-      descriptor_1.convertTo(descriptor_1, CV_32F);
+  for (size_t idx = 1; idx != images.size(); ++idx) {
+    std::vector<cv::KeyPoint> keypoints;
+    cv::Mat descriptor;
+    detdes_ptr->detectAndCompute(images[idx], mask, keypoints,
+                                 descriptor, false);
+    if(descriptor.type() != CV_32F) {
+      descriptor.convertTo(descriptor, CV_32F);
     }
 
-    std::vector<cv::KeyPoint> keypoints_2;
-    detdes_ptr->detect(image_2, keypoints_2);
-
-    cv::Mat descriptor_2;
-    detdes_ptr->compute(image_2, keypoints_2, descriptor_2);
-    if(descriptor_2.type() != CV_32F) {
-      descriptor_2.convertTo(descriptor_2, CV_32F);
-    }
-
-    cv::FlannBasedMatcher matcher;
+    // Vector of vectors because each query will have k matches
     std::vector<std::vector<cv::DMatch>> raw_matches;
-    matcher.knnMatch(descriptor_1, descriptor_2, raw_matches, 2);
+    matcher.knnMatch(descriptor, raw_matches, 2);
+
+    // Use ratio matching
+    std::vector<cv::DMatch> ratio_matches;
+    std::vector<cv::Mat> new_descriptors;
+    for (size_t idx = 0; idx != raw_matches.size(); ++idx) {
+      if (raw_matches[idx][0].distance < 0.45*raw_matches[idx][1].distance) {
+        ratio_matches.push_back(raw_matches[idx][0]);
+      } else {
+        new_descriptors.push_back(descriptor.row(raw_matches[idx][0].queryIdx));
+        all_descriptors.push_back(descriptor.row(raw_matches[idx][0].queryIdx));
+      }
+    }
+
+    // Add new matches to the matcher
+    matcher.add(new_descriptors);
+  }
+
+  std::cout << "  Know of " << all_descriptors.size() << " descriptors" << std::endl;
+
+  // Pass 2 match them against all known "unique" descriptors
+  std::cout << "  Pass 2" << std::endl;
+  matcher.clear();
+  matcher.add(all_descriptors);
+  std::map<cv::Mat, std::vector<cv::KeyPoint>> feature_map;
+  for (size_t idx = 0; idx != images.size(); ++idx) {
+    std::vector<cv::KeyPoint> keypoints;
+    cv::Mat descriptor;
+    detdes_ptr->detectAndCompute(images[idx], mask, keypoints,
+                                 descriptor, false);
+    if(descriptor.type() != CV_32F) {
+      descriptor.convertTo(descriptor, CV_32F);
+    }
+
+    // Vector of vectors because each query will have k matches
+    std::vector<std::vector<cv::DMatch>> raw_matches;
+    matcher.knnMatch(descriptor, raw_matches, 2);
 
     // Use ratio matching
     std::vector<cv::DMatch> ratio_matches;
     for (size_t idx = 0; idx != raw_matches.size(); ++idx) {
       if (raw_matches[idx][0].distance < 0.45*raw_matches[idx][1].distance) {
         ratio_matches.push_back(raw_matches[idx][0]);
+        feature_map[all_descriptors[raw_matches[idx][0].trainIdx]]
+          .push_back(keypoints[raw_matches[idx][0].queryIdx]);
       }
     }
-
-//    // Get rid of impossible matches
-//    std::vector<cv::DMatch> filter_matches;
-//    std::copy_if(ratio_matches.begin(), ratio_matches.end(),
-//                 std::back_inserter(filter_matches),
-//      [&](const cv::DMatch &match) {
-//      return std::abs(keypoints_1[match.queryIdx].pt.y -
-//                      keypoints_2[match.trainIdx].pt.y) < 12;
-//      });
-//    std::cout << "Removed " << ratio_matches.size() - filter_matches.size() << " matches\n";
-
-    cv::Mat color_image;
-    cv::cvtColor(image_1, color_image, cv::COLOR_GRAY2RGB);
-    for (const auto &match : ratio_matches) {
-      auto pt1 = keypoints_1[match.queryIdx].pt;
-      auto pt2 = keypoints_2[match.trainIdx].pt;
-      cv::circle(color_image, pt1, 3, {0, 0, 255});
-      cv::line(color_image, pt1, pt2, {0, 0, 255});
-      if (std::abs(pt2.x - pt1.x) > 4) {
-        motion_vecs.push_back({{pt1.x, pt1.y, pt2.x-pt1.x, pt2.y-pt1.y}});
-      }
-    }
-    cv::imshow("image", color_image);
   }
+
+  std::vector<std::array<double, 2>> feature_points;
+  for (const auto &kv : feature_map) {
+    for (const auto &keypoint : kv.second) {
+      feature_points.push_back({{keypoint.pt.x, keypoint.pt.y}});
+    }
+  }
+
+  std::vector<std::array<double, 4>> motion_vecs;
+
+//  cv::Mat color_image;
+//  cv::cvtColor(image_1, color_image, cv::COLOR_GRAY2RGB);
+//  for (const auto &match : ratio_matches) {
+//    auto pt1 = keypoints_1[match.queryIdx].pt;
+//    auto pt2 = keypoints_2[match.trainIdx].pt;
+//    cv::circle(color_image, pt1, 3, {0, 0, 255});
+//    cv::line(color_image, pt1, pt2, {0, 0, 255});
+//    if (std::abs(pt2.x - pt1.x) > 4) {
+//      motion_vecs.push_back({{pt1.x, pt1.y, pt2.x-pt1.x, pt2.y-pt1.y}});
+//    }
+//  }
+//  cv::imshow("image", color_image);
 
   gimx.SendControl(XboneControl());
 
@@ -432,6 +531,9 @@ int main (int argc, char **argv) {
 //  auto sendVec = [&] (const std::vector<double> &vec, const std::string &name) {
 //    mpl.SendData(NumpyArray(name, vec));
 //  };
+
+  mpl.SendData(NumpyArray("points", (double*)feature_points.data(),
+                          feature_points.size(), 2));
 
   mpl.SendData(NumpyArray("mv", (double*)motion_vecs.data(),
                           motion_vecs.size(), 4));
