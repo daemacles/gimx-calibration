8498ea58fa80c08ffa9a2f117b5756c86a11489a
James Youngquist <jay@cs.uw.edu>
2016-01-02 16:01:17 -0800

    Both axes working, but jittery

--- src/control.cc
+++ src/control.cc
@@ -1,6 +1,7 @@
 #include <sys/time.h>
 
 #include <iostream>
+#include <map>
 #include <random>
 
 #include <QtWidgets>
@@ -13,7 +14,7 @@ std::mt19937 rand_gen(rd());
 int GetControl (double phi) {
   double a = 0.00136871;
   double c = 0.00017554;
-  double d = -0.0057529;
+  double d = -0.0057529 + 0.0043841921696713944;
   double inflection = 0.22262857715;
   double fastest = 0.939737022742;
 
@@ -34,6 +35,44 @@ int GetControl (double phi) {
   }
 }
 
+class AxisControl {
+public:
+  AxisControl (double upper_thresh, double range) :
+      accum_(0.0),
+      prev_(0.0),
+      upper_thresh_(upper_thresh),
+      range_(range),
+      exponent_(1.6)
+  {
+  }
+
+  void Accumulate (double value) {
+    double diff = value - prev_;
+    prev_ = value;
+    if (std::abs(diff) < upper_thresh_) {
+      accum_ += diff;
+    }
+  }
+
+  int GetAxisValue (void) {
+    double sign = std::signbit(accum_) ? -1 : 1;
+    double value = std::min(std::abs(accum_), range_) / range_;
+    value = sign*std::pow(std::abs(value), exponent_) * 0.94 * 1;
+
+    accum_ = 0;
+
+    return GetControl(value);
+  }
+
+private:
+  double accum_;
+  double prev_;
+  double upper_thresh_;
+  double range_;
+  double exponent_;
+};
+
+
 class GimxControl : public QWidget {
 public:
   GimxControl (QWidget *parent = 0);
@@ -42,23 +81,27 @@ protected:
   void mouseMoveEvent (QMouseEvent *event) Q_DECL_OVERRIDE;
   void mouseReleaseEvent (QMouseEvent *event) Q_DECL_OVERRIDE;
   void mousePressEvent (QMouseEvent *event) Q_DECL_OVERRIDE;
+  void keyPressEvent (QKeyEvent *event) Q_DECL_OVERRIDE;
   void keyReleaseEvent (QKeyEvent *event) Q_DECL_OVERRIDE;
   void timerEvent (QTimerEvent *event) Q_DECL_OVERRIDE;
 
 private:
   bool mouse_grabbed_;
   GimxConnection gimx_;
-  double accum_;
+  AxisControl x_axis_;
+  AxisControl y_axis_;
+  std::map<Qt::Key, bool> keys_pressed_;
 };
 
 GimxControl::GimxControl (QWidget *parent) :
     QWidget(parent),
     mouse_grabbed_(false),
     gimx_("localhost", 7799),
-    accum_(0.0)
+    x_axis_(30, 25),
+    y_axis_(30, 25)
 {
   this->setMouseTracking(true);
-  this->startTimer(10);
+  this->startTimer(4);
   gimx_.Connect();
 }
 
@@ -68,24 +111,25 @@ void GimxControl::mousePressEvent(QMouseEvent *event) {
   }
 }
 
-void GimxControl::mouseMoveEvent(QMouseEvent *event) {
-  static int last_x = 0;
 
+void GimxControl::mouseMoveEvent(QMouseEvent *event) {
   if (mouse_grabbed_) {
     if (event->x() < this->width()/4 ||
         event->x() > 3*this->width()/4) {
       auto cursor = this->cursor();
       cursor.setPos(this->mapToGlobal(QPoint(this->width()/2,
+                                             cursor.pos().y())));
+      this->setCursor(cursor);
+    } else if (event->y() < this->height()/4 ||
+               event->y() > 3*this->height()/4) {
+      auto cursor = this->cursor();
+      cursor.setPos(this->mapToGlobal(QPoint(cursor.pos().x(),
                                              this->height()/2)));
       this->setCursor(cursor);
     }
 
-    int diff = event->globalX() - last_x;
-    last_x = event->globalX();
-
-    if (std::abs(diff) < this->width()/4) {
-      accum_ += diff;
-    }
+    x_axis_.Accumulate(event->globalX());
+    y_axis_.Accumulate(event->globalY());
   }
 }
 
@@ -95,7 +139,13 @@ void GimxControl::mouseReleaseEvent(QMouseEvent *event) {
   }
 }
 
+void GimxControl::keyPressEvent(QKeyEvent *event) {
+  keys_pressed_[event->key()] = true;
+}
+
 void GimxControl::keyReleaseEvent(QKeyEvent *event) {
+  keys_pressed_[event->key()] = false;
+
   switch(event->key()) {
   case Qt::Key_Q:
     QCoreApplication::exit(0);
@@ -117,19 +167,10 @@ void GimxControl::keyReleaseEvent(QKeyEvent *event) {
 }
 
 void GimxControl::timerEvent (QTimerEvent *event) {
-  double sign = std::signbit(accum_) ? -1 : 1;
-  double range = 20;
-  double value = std::min(std::abs(accum_), range) / range;
-  value = sign*std::pow(std::abs(value), 1.6) * 0.94 * 1;
-
-  int x_control = GetControl(value);
-
   XboneControl ctl;
-  ctl.right_stick.x = x_control;
-  ctl.right_stick.y = 0;
+  ctl.right_stick.x = x_axis_.GetAxisValue();
+  ctl.right_stick.y = y_axis_.GetAxisValue();
   gimx_.SendControl(ctl);
-
-  accum_ = 0;
 }
 
 int main (int argc, char *argv[]) {
